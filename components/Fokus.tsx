0f7b10e981c95f16d7683256f398b4dbf6e87eb4
Eirik Kirkeli <knirkeli88@msn.com>
2024-08-08 14:04:24 +0200

    fixed client error

--- components/Fokus.tsx
+++ components/Fokus.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import { boardGame } from "../app/interface";
-import { client as sanityClient } from '../sanity/lib/client';
+import { client as sanityClient } from "../sanity/lib/client";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
 // import { urlForImage} from "../sanity/lib/image"
-import imageUrlBuilder from '@sanity/image-url';
+import imageUrlBuilder from "@sanity/image-url";
 
 // Initialize the image URL builder
 const builder = imageUrlBuilder(sanityClient);
@@ -15,11 +17,11 @@ function urlForImage(image) {
 
 // Custom loader function for Next.js Image component
 const myLoader = ({ src, width, quality }) => {
-  return `${src}?w=${width}&q=${quality || 75}`
-}
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
 
 async function getData() {
-  const query =`*[_type == "boardgame" && tag =="Fokus"] {
+  const query = `*[_type == "boardgame" && tag =="Fokus"] {
     name,
       image,
   }`;
@@ -27,7 +29,6 @@ async function getData() {
   const data = await sanityClient.fetch(query);
   return data;
 }
-
 
 export default async function Fokus() {
   const data: boardGame[] = await getData();
@@ -39,20 +40,18 @@ export default async function Fokus() {
       {data.map((post, idx) => (
         <Card key={idx}>
           <CardContent className="mt-5">
-            <h3>
-              {post.name}
-            </h3>
+            <h3>{post.name}</h3>
             <Image
-            loader={myLoader}
+              loader={myLoader}
               src={urlForImage(post.image).url()}
               alt={post.image.alt}
               width={200}
               height={200}
               className="rounded-t-lg h-[200px] object-cover"
-              />
+            />
           </CardContent>
         </Card>
       ))}
     </div>
-  )
+  );
 }
