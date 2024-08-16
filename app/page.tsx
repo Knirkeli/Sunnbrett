9fb7aa2563a531a0df80602a3e5156e4c81f972b
Eirik Kirkeli <knirkeli88@msn.com>
2024-08-16 22:31:50 +0200

    velkommen and carousel styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
+// "use client";
+// import Image from "next/image";
+// import Velkommen from "../components/content/Velkommen";
+// import Fokus from "../components/content/Fokus";
+// import Header from "@/components/content/header";
+// import ImageCarousel from "@/components/content/imageCarousel";
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <Header />
+//       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5">
+//         <Velkommen />
+//         <ImageCarousel />
+//       </div>
+//       <div className="mt-5">
+//         <Fokus />
+//       </div>
+//     </div>
+//   );
+// }
+
 "use client";
 import Image from "next/image";
 import Velkommen from "../components/content/Velkommen";
@@ -10,10 +32,14 @@ export default function Home() {
     <div>
       <Header />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5">
-        <Velkommen />
-        <ImageCarousel />
+        <div className="flex justify-center">
+          <Velkommen />
+        </div>
+        <div className="flex justify-center">
+          <ImageCarousel />
+        </div>
       </div>
-      <div className="mt-5">
+      <div className="my-5">
         <Fokus />
       </div>
     </div>
--- components/content/Velkommen.tsx
+++ components/content/Velkommen.tsx
@@ -1,25 +1,6 @@
 // import { velkommenPost } from "../../app/interface";
-// import { client } from "../../sanity/lib/client";
+// import { getData } from "../Fetch/FetchVelkommen";
 // import { Card, CardContent } from "../ui/card";
-
-// async function getData() {
-//   const query = `*[_type == "post" && title == "Velkommen til oss"]{
-//     _id,
-//     title,
-//     body[]{
-//       ...,
-//       children[]{
-//         ...
-//       },
-//       markDefs[]{
-//         ...
-//       }
-//     }
-//   }`;
-
-//   const data = await client.fetch(query);
-//   return data;
-// }
 
 // export default async function Velkommen() {
 //   const data: velkommenPost[] = await getData();
@@ -67,38 +48,40 @@ import { Card, CardContent } from "../ui/card";
 export default async function Velkommen() {
   const data: velkommenPost[] = await getData();
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5">
-      {data.map((post, idx) => (
-        <Card key={idx} className="w-11/12 mx-auto mb-5">
-          <CardContent className="mt-5 p-4 rounded shadow-xl">
-            <h3 className="text-2xl font-bold mb-4">{post.title}</h3>
-            {post.body.map((block, blockIdx) => (
-              <div key={blockIdx}>
-                {block.children.map((child, childIdx) => {
-                  const link = block.markDefs.find(
-                    (def) => def._key === child.marks[0]
-                  );
-                  return link && link._type === "link" ? (
-                    <a
-                      key={childIdx}
-                      href={link.href}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="text-blue-500 underline"
-                    >
-                      {child.text}
-                    </a>
-                  ) : (
-                    <p key={childIdx} className="text-base mb-2">
-                      {child.text}
-                    </p>
-                  );
-                })}
-              </div>
-            ))}
-          </CardContent>
-        </Card>
-      ))}
+    <div className="w-3/4 lg-w-2/3 flex mx-auto my-auto">
+      <div className="w-11/12">
+        {data.map((post, idx) => (
+          <Card key={idx} className="mb-5">
+            <CardContent className="mt-5 p-4 rounded shadow-xl">
+              <h3 className="text-2xl font-bold mb-4">{post.title}</h3>
+              {post.body.map((block, blockIdx) => (
+                <div key={blockIdx}>
+                  {block.children.map((child, childIdx) => {
+                    const link = block.markDefs.find(
+                      (def) => def._key === child.marks[0]
+                    );
+                    return link && link._type === "link" ? (
+                      <a
+                        key={childIdx}
+                        href={link.href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-blue-500 underline"
+                      >
+                        {child.text}
+                      </a>
+                    ) : (
+                      <p key={childIdx} className="text-base mb-2">
+                        {child.text}
+                      </p>
+                    );
+                  })}
+                </div>
+              ))}
+            </CardContent>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 }
