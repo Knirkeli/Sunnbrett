dd2d1160bf010198f75258489d7396c6c2cf34f0
Eirik Kirkeli <knirkeli88@msn.com>
2024-09-13 15:45:13 +0200

    Fikset styling og possisjonering på page.tsx componenter

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,3 @@
-// "use client";
-// import Image from "next/image";
-// import Velkommen from "../components/content/Velkommen";
-// import Fokus from "../components/content/Fokus";
-// import Header from "@/components/content/header";
-// import ImageCarousel from "@/components/content/imageCarousel";
-// import Footer from "@/components/content/footer";
-
-// export default function Home() {
-//   return (
-//     <div>
-//       <Header />
-//       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5 shadow-xl pb-5">
-//         <div className="flex justify-center">
-//           <Velkommen />
-//         </div>
-//         <div className="flex justify-center">
-//           <ImageCarousel />
-//         </div>
-//       </div>
-//       <div className="pt-5 flex pb-[112px]">
-//         <Fokus />
-//       </div>
-//       <Footer />
-//     </div>
-//   );
-// }
-
 "use client";
 import Image from "next/image";
 import Velkommen from "../components/content/Velkommen";
@@ -38,11 +10,11 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-5 shadow-xl pb-5">
-        <div className="flex justify-center items-center h-full">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-5 shadow-xl pb-5">
+        <div className="flex justify-center items-center h-full lg:col-span-1">
           <Velkommen />
         </div>
-        <div className="flex justify-center items-center h-full">
+        <div className="flex justify-center items-center h-full lg:col-span-2">
           <ImageCarousel />
         </div>
       </div>
