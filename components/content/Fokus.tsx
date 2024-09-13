2eae49e67d3bc86b259129461d1242b957f309aa
Eirik Kirkeli <knirkeli88@msn.com>
2024-09-13 15:11:48 +0200

    Fikset forskjellige størelser på fokus kort

--- components/content/Fokus.tsx
+++ components/content/Fokus.tsx
@@ -1,73 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { boardGame } from "../../app/interface";
-// import { getData } from "../Fetch/FetchFokus";
-// import GameDialog from "../content/gameDialog";
-// import "../../app/globals.css";
-
-// export default function Fokus() {
-//   const [data, setData] = useState<boardGame[]>([]);
-//   const [selectedGame, setSelectedGame] = useState<boardGame | null>(null);
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       const result = await getData();
-//       setData(result);
-//     }
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className="text-2xl text-center w-2/3 mx-auto my-auto">
-//       <h3>Spel i Fokus</h3>
-//       <div className="grid grid-cols-1 lg:grid-cols-2 mt-5 gap-8">
-//         {data.map((post, idx) => (
-//           <GameDialog
-//             key={idx}
-//             game={post}
-//             selectedGame={selectedGame}
-//             setSelectedGame={setSelectedGame}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// import { useEffect, useState } from "react";
-// import { boardGame } from "../../app/interface";
-// import { getData } from "../Fetch/FetchFokus";
-// import GameDialog from "../content/gameDialog";
-// import "../../app/globals.css";
-
-// export default function Fokus() {
-//   const [data, setData] = useState<boardGame[]>([]);
-//   const [selectedGame, setSelectedGame] = useState<boardGame | null>(null);
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       const result = await getData();
-//       setData(result);
-//     }
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className="text-2xl text-center w-10/12 lg:w-4/5 xl:w-10/12 mx-auto my-auto">
-//       <h3>Spel i Fokus</h3>
-//       <div className="grid grid-cols-1 lg:grid-cols-4 mt-5 mb-5 gap-6">
-//         {data.map((post, idx) => (
-//           <GameDialog
-//             key={idx}
-//             game={post}
-//             selectedGame={selectedGame}
-//             setSelectedGame={setSelectedGame}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
 import { useEffect, useState } from "react";
 import { boardGame } from "../../app/interface";
 import { getData } from "../Fetch/FetchFokus";
@@ -89,15 +19,14 @@ export default function Fokus() {
   return (
     <div className="text-2xl text-center w-10/12 lg:w-4/5 xl:w-10/12 mx-auto my-auto">
       <h3>Spel i Fokus</h3>
-      <div className="flex flex-wrap justify-center mt-5 mb-5 gap-6">
-        {data.map((post, idx) => (
-          <div key={idx} className="w-full lg:w-1/4">
-            <GameDialog
-              game={post}
-              selectedGame={selectedGame}
-              setSelectedGame={setSelectedGame}
-            />
-          </div>
+      <div className="mx-auto my-auto pt-4 pb-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {data.map((game, idx) => (
+          <GameDialog
+            key={idx}
+            game={game}
+            selectedGame={selectedGame}
+            setSelectedGame={setSelectedGame}
+          />
         ))}
       </div>
     </div>
